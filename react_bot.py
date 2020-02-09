b0d5a684b15c1d88675dea5d503607646b0333f7
dink <butth3r0@gmail.com>
2020-02-09 15:35:42 +1300

    fd

--- react_bot.py
+++ react_bot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import sqlite3
 import os
+import requests
+import io
 
 #set up database
 conn = sqlite3.connect('example.db')
@@ -16,24 +18,27 @@ def check_table_exists():
     else:
         print("no table exists, creating")
         csr.execute('''CREATE TABLE reacts
-                    (alias text, url text)
+                    (alias text, url text, file blob)
                     ''')
 
-def insert(alias, url):
+def insert_text_to_db(alias, url):
     csr.execute('''INSERT INTO reacts
     (alias, url) VALUES
     (?,?)
     ''',(alias, url))
 
-def retrieve(alias):
-    csr.execute("""SELECT alias, url FROM reacts WHERE alias = ?""",(alias,))
-    result = csr.fetchall()
-    if result == []:
-        return False
+def retrieve_from_db(alias):
+    csr.execute("""SELECT alias, url, file FROM reacts WHERE alias = ?""",(alias,))
+    db_result = csr.fetchall()
+    if db_result == []:
+        result = False
+    elif db_result[0][2] != None: #if not empty
+        result = db_result[0][2]
     else:
-        return result[0][1]
+        result = db_result[0][1]
+    return result
 
-def check_exists(alias):
+def check_exists_in_db(alias):
     """return true if exists"""
     csr.execute("""SELECT alias, url FROM reacts WHERE alias = ?""", (alias,))
     result = csr.fetchall()
@@ -42,22 +47,33 @@ def check_exists(alias):
     else:
         return True
 
-check_table_exists() #check table exists otherwise create one
+def check_message_type(message): #check if embed, attachment, both, or neither
+    attach = message.attachments
+    embed = message.embeds
+    if (embed == []) and (attach == []):
+        result = "nothing"
+    elif (embed != []) and (attach != []):
+        result = "both"
+    elif embed != []:
+        result = "embed"
+    elif attach != []:
+        result = "attachment"
+    return result
 
-client = commands.Bot(command_prefix = ".")
+def get_message_attachment(message):
+    file_url = message.attachments[0].url
+    request = requests.get(file_url)
+    file_data = request.content
+    return file_data
 
-@client.event
-async def on_ready():
-    print("logged in")
-
-@client.command()
-async def ping(ctx):
-    """Bot says 'hi'"""
-    await ctx.send("React_bot says 'hi'")
+def insert_blob_to_db(alias, file_data):
+    csr.execute("""INSERT INTO reacts
+    (alias, file) VALUES
+    (?, ?)
+    """,(alias, file_data))
 
 
-@client.command()
-async def add(ctx, *args):
+async def add_url(ctx, *args):
     """.add <keyword> <url> . Will bind the <keyword> with the <url>. Summon using .gif"""
     """add alias-string to database of space-separated alias-string pairs. only accept one space as space is the delimiter
     only allow unique entries, detect spaces greater or less than 1. return error messages when spaced format incorrect
@@ -66,22 +82,68 @@ async def add(ctx, *args):
     added_string = " ".join(args)#put all arges into one string, for the case of multiple words
     space_count = added_string.count(" ") #count spaces in added_string
     added_string_alias = added_string.split(" ")[0]  # get word before space
-    added_string_url = added_string.split(" ")[1]
-
     if space_count > 1:
         await ctx.send("error: excess spaces. use only one space. format: <alias> <space> <string>")
-        return
     elif space_count == 0:
         await ctx.send("no space detected, please use format: <alias> <space> <string>")
-        return
-    elif check_exists(added_string_alias) == True:
+    elif check_exists_in_db(added_string_alias):
         await ctx.send("alias already exists, nothing added")
-        return
     else:
-        insert(added_string_alias, added_string_url)
+        added_string_url = added_string.split(" ")[1]  # get word after the first space
+        insert_text_to_db(added_string_alias, added_string_url)
         conn.commit()  # write changes to db
         await ctx.send("alias added")
-        return
+    return
+
+async def add_file(ctx, *args):
+    """.add <keyword> <url> . Will bind the <keyword> with the <url>. Summon using .gif"""
+    """add alias-string or alias-blob to database of alias-string and alias-blob pairs. For urls only accept one space as space is the delimiter
+    only allow unique entries, detect spaces greater or less than 1. return error messages when spaced format incorrect
+    """
+    found = False
+    added_string = " ".join(args)  # put all args into one string, for the case of multiple words
+    space_count = added_string.count(" ") #count spaces in added_string
+    if space_count > 0:
+        await ctx.send("error: try again without extra spaces. example: .add funnycat")
+    elif check_exists_in_db(added_string):
+        await ctx.send("alias already exists, nothing added")
+    else:
+        file_data = get_message_attachment(ctx.message)
+        if len(file_data) > 5000000:
+            await ctx.send("File larger than max filesize of 5MB")
+        elif len(file_data) < 5000:
+            await ctx.send("File smaller than minimum size of 5KB")
+        else:
+            insert_blob_to_db(added_string, file_data)
+            conn.commit()  # write changes to db
+            await ctx.send("alias added")
+    return
+
+check_table_exists() #check table exists otherwise create one
+
+client = commands.Bot(command_prefix = ".")
+
+@client.event
+async def on_ready():
+    channel = client.get_channel(671911325579870210) #specific channel
+    await channel.send("React bot ready")
+    print("logged in")
+
+@client.command()
+async def ping(ctx):
+    """Bot says 'hi'"""
+    await ctx.send("React_bot: pong")
+
+@client.command()
+async def add(ctx, *args):
+    if check_message_type(ctx.message) == "nothing":
+        await ctx.send("No image attachment or image url detected")
+    elif check_message_type(ctx.message) == "both":
+        await ctx.send("Image attachment AND url BOTH detected, please use one or the other. Not both.")
+    elif check_message_type(ctx.message) == "embed":
+        await add_url(ctx, *args)
+    elif check_message_type(ctx.message) == "attachment":
+        await add_file(ctx, *args)
 
 @client.command()
 async def gif(ctx, *args):
@@ -91,13 +153,16 @@ async def gif(ctx, *args):
     if find_string.count(" ") != 0:#check for spaces
         await ctx.send("error please omit spaces")
         return
-    if check_exists(find_string) == False:
+    if check_exists_in_db(find_string) == False:
         await ctx.send("alias not found")
         return
-    else:
-        found_alias = retrieve(find_string)
+    found_alias = retrieve_from_db(find_string)
+    if isinstance(found_alias, str): #if it's a url aka string
         await ctx.send(found_alias)
-
+    elif isinstance(found_alias, bytes):#if it's bytes aka a picture file
+        filelike_object = io.BytesIO(found_alias) #convert to a object similar to a file
+        discord_file_object = discord.File(filelike_object, "react.gif")
+        await ctx.send(file=discord_file_object)
 # @client.command()
 # async def help2(ctx):
 #     await ctx.send("Two modules currently."
